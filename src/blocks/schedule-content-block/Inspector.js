f5d8d7730c49dd4f5a4069044089cf3bb8a42c44
goiblas <jesus.designer@gmail.com>
2020-02-25 14:38:47 +0100

    fix type error

--- src/blocks/schedule-content-block/Inspector.js
+++ src/blocks/schedule-content-block/Inspector.js
@@ -4,7 +4,6 @@ import { PanelBody,  DateTimePicker,  RadioControl } from '@wordpress/components
 import { __ } from '@wordpress/i18n';
 
 export default class Inspector extends Component {
-
     render() {
         const { attributes, setAttributes } = this.props;
         const { date, hiddenStart } = attributes;
@@ -18,24 +17,24 @@ export default class Inspector extends Component {
             setAttributes({date: timestamp});
         }
         return (
-	<InspectorControls>
-		<PanelBody>
-			<DateTimePicker
-				currentDate={ date }
-				onChange={ onChangeDate }
-                />
-		</PanelBody>
-		<PanelBody>
-			<RadioControl
-				selected={ radioValue }
-				options={ [
-                        { label: __('Start as visible', 'schedule-content-block'), value: 'startAsVisible' },
-                        { label:  __('Start as hidden', 'schedule-content-block'), value: 'startAsHidden' },
-                    ] }
-				onChange={ onChangeRatio } 
-                />
-		</PanelBody>
-	</InspectorControls>
+            <InspectorControls>
+                <PanelBody>
+                    <DateTimePicker
+                        currentDate={ date }
+                        onChange={ onChangeDate }
+                        />
+                </PanelBody>
+                <PanelBody>
+                    <RadioControl
+                        selected={ radioValue }
+                        options={ [
+                                { label: __('Start as visible', 'schedule-content-block'), value: 'startAsVisible' },
+                                { label:  __('Start as hidden', 'schedule-content-block'), value: 'startAsHidden' },
+                            ] }
+                        onChange={ onChangeRatio } 
+                        />
+                </PanelBody>
+            </InspectorControls>
         );
     }
 }
